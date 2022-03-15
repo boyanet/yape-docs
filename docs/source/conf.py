aa1f4d9569a48512335cfe6de296c02b9baf54a0
Gorachya <38173958+Gorachya@users.noreply.github.com>
2022-03-15 14:11:59 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,10 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['../../../_static']
+html_static_path = ['../../_static']
 html_logo = "logo.svg"
 html_theme_options = {
-    'logo_only': False,
+    'logo_only': True,
     'display_version': False,
 }
 
