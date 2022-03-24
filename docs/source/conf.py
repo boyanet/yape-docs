460b2f2641a613ace1942c161eeb156aa4e1f99e
Gorachya <38173958+Gorachya@users.noreply.github.com>
2022-03-24 14:36:16 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = "logo.svg"
+html_logo = "logo.png"
 html_theme_options = {
     'logo_only': False,
     'display_version': False,
